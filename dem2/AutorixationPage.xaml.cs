a42c2d06562d8902c80428c8a1cab817cb1ac9dd
Admin_Mark <mrag.forgames@gmail.com>
2024-05-20 21:46:45 +0300

    Добавьте файлы проекта.

---
+++ dem2/AutorixationPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dem2
+{
+	/// <summary>
+	/// Логика взаимодействия для AutorixationPage.xaml
+	/// </summary>
+	public partial class AutorixationPage : Page
+	{
+		public AutorixationPage()
+		{
+			InitializeComponent();
+		}
+
+		private void EntryButton_Click(object sender, RoutedEventArgs e)
+		{
+			Manager.MainFrame.Navigate(new ZajavkiPage());
+        }
+
+		private void ReginButton_Click(object sender, RoutedEventArgs e)
+		{
+			Manager.MainFrame.Navigate(new RegistrationPage());
+        }
+    }
+}
---
+++ dem2/ZajavkiPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dem2
+{
+	/// <summary>
+	/// Логика взаимодействия для ZajavkiPage.xaml
+	/// </summary>
+	public partial class ZajavkiPage : Page
+	{
+		public ZajavkiPage()
+		{
+			InitializeComponent();
+		}
+
+		private void EditButton_Click(object sender, RoutedEventArgs e)
+		{
+			Manager.MainFrame.Navigate(new NewZajavkaPage());
+        }
+
+		private void AddButton_Click(object sender, RoutedEventArgs e)
+		{
+			Manager.MainFrame.Navigate(new NewZajavkaPage());
+		}
+
+		private void DeleteButton_Click(object sender, RoutedEventArgs e)
+		{
+
+        }
+    }
+}
